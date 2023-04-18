bbf07a27541dadf98fa1b1f5835363143e9ee430
RoinerRocha <rochabozaroiner@gmail.com>
2023-04-18 12:55:44 -0600

    changes in users

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const user = new Schema({
   fname: { type: String },
   lname: { type: String },
+  phone: {type: String},
   email: {type: String, unique:true},
   password: {type: String},
   role: {type: String}
